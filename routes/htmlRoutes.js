e60a39abd8af2680fb5666fedfa9f37e4c5a50ed
Christopher Hayashi <Chrisrhayashi@gmail.com>
2020-02-13 20:27:58 -0800

    setup routes and imported navbar and portfolio cards

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var path = require("path");
+
+var router = express.Router();
+
+router.get("/", function(req, res) {  
+    res.render("home");
+});
+
+router.get("/portfolio", function(req, res) {
+    res.render("portfolio");
+});
+
+router.get("/contact", function(req, res) {
+    res.render("contact");
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -17,8 +17,8 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-// var routes = require("./routes/htmlRoutes");
-// app.use(routes);
+var routes = require("./routes/htmlRoutes");
+app.use(routes);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
